fcfcd2acc3c4709692054408ce192c9aee010a49
lbreguet <louis-w@live.com>
2017-01-25 10:51:17 -0500

    Buttons and auth have been added.
    -time to start on the game engine.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,6 +3,7 @@
 const setAPIOrigin = require('../../lib/set-api-origin');
 const config = require('./config');
 
+
 $(() => {
   setAPIOrigin(location, config);
 });
@@ -11,4 +12,9 @@ $(() => {
 // const example = require('./example');
 
 // use require without a reference to ensure a file is bundled
-require('./example');
+const authEvents = require('./auth/events.js');
+
+// On document ready
+$(() => {
+  authEvents.addHandlers();
+});
