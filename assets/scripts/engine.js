5123a4249c2ab708427907a384874a5c3794abd0
lbreguet <louis-w@live.com>
2017-01-26 17:02:28 -0500

    Game working.
    -Game API needs working.
    -new game button not working.

--- assets/scripts/engine.js
+++ assets/scripts/engine.js
@@ -1,80 +1,147 @@
 'use strict';
 
-let board = [
-  '','','',
-  '','','',
-  '','',''
-];
+const board = ['', '', '', '', '', '', '', '', ''];
 
-// let player1 = 'X';
-// let player2 = 'O';
+let player1 = 'X';
+let player2 = 'O';
 
-let currentPlayer = 'X';
+let currentPlayer = player1;
 
-const checkWins = function () {
-  if (board[0] === 'X' && board[1] === 'X' && board[2] === 'X' ||
-  board[3] === 'X' && board[4] === 'X' && board[5] === 'X' ||
-  board[6] === 'X' && board[7] === 'X' && board[8] === 'X' ||
-  board[0] === 'X' && board[4] === 'X' && board[8] === 'X' ||
-  board[2] === 'X' && board[4] === 'X' && board[6] === 'X' ||
-  board[0] === 'X' && board[3] === 'X' && board[6] === 'X' ||
-  board[1] === 'X' && board[4] === 'X' && board[7] === 'X' ||
-  board[2] === 'X' && board[5] === 'X' && board[8] === 'X') {
-    $('.win').text('Player 1 is the Winner!');
-  } else if (board[0] === 'O' && board[1] === 'O' && board[2] === 'O' ||
-  board[3] === 'O' && board[4] === 'O' && board[5] === 'O' ||
-  board[6] === 'O' && board[7] === 'O' && board[8] === 'O' ||
-  board[0] === 'O' && board[4] === 'O' && board[8] === 'O' ||
-  board[2] === 'O' && board[4] === 'O' && board[6] === 'O' ||
-  board[0] === 'O' && board[3] === 'O' && board[6] === 'O' ||
-  board[1] === 'O' && board[4] === 'O' && board[7] === 'O' ||
-  board[2] === 'O' && board[5] === 'O' && board[8] === 'O') {
-    $('.win').text('Player 2 is the Winner!');
-  } else if (board.include('') === false){
-    $('.win').text('Cats Game!');
-    console.log('Cats Game!');
-  }
-};
 
-const turns = function(index) {
-  if (board[index] === '') {
-    board[index] = currentPlayer;
-    checkWins();
-    if (currentPlayer === 'X') {
-      currentPlayer = 'O';
-    } else if (currentPlayer === 'O'){
-      currentPlayer = 'X';
-    }
-  } else {
-    console.log('Error');
-  }
-};
 
-$('.box').on('click', function(event) {
-  if ($(event.target).text() === '') {
-    $(event.target).text(currentPlayer);
-  } turns(event.target.id);
-  console.log(board);
-});
-
-const resetGame = function () {
+const resetGameBoard = function() {
   for (let i = 0; i < board.length; i++) {
     board[i] = '';
     $('.box').text('');
     $('.win').text('');
     currentPlayer = 'X';
-  } $('.box').on('click', function(event) {
+
+
+  }
+  $('.box').on('click', function(event) {
     if ($(event.target).text() === '') {
       $(event.target).text(currentPlayer);
     } turns(event.target.id);
     console.log(board);
+
   });
 };
+
+
+
+ $('#new-game').on('submit', function() {
+   resetGameBoard();
+});
+
+const checkWins = function() {
+  if (
+    board[0] === player1 && board[1] === player1 && board[2] === player1 ||
+    board[3] === player1 && board[4] === player1 && board[5] === player1 ||
+    board[6] === player1 && board[7] === player1 && board[8] === player1 ||
+    board[0] === player1 && board[4] === player1 && board[8] === player1 ||
+    board[2] === player1 && board[4] === player1 && board[6] === player1 ||
+    board[0] === player1 && board[3] === player1 && board[6] === player1 ||
+    board[1] === player1 && board[4] === player1 && board[7] === player1 ||
+    board[2] === player1 && board[5] === player1 && board[8] === player1
+  ) {
+    $('.win').text("Player 1 Won!");
+    console.log('Player 1 Won!');
+    endGame();
+  } else if (
+    board[0] === player2 && board[1] === player2 && board[2] === player2 ||
+    board[3] === player2 && board[4] === player2 && board[5] === player2 ||
+    board[6] === player2 && board[7] === player2 && board[8] === player2 ||
+    board[0] === player2 && board[4] === player2 && board[8] === player2 ||
+    board[2] === player2 && board[4] === player2 && board[6] === player2 ||
+    board[0] === player2 && board[3] === player2 && board[6] === player2 ||
+    board[1] === player2 && board[4] === player2 && board[7] === player2 ||
+    board[2] === player2 && board[5] === player2 && board[8] === player2
+  ) {
+    $('.win').text("Player 2 Won!");
+    console.log('Player 2 Won!');
+    endGame();
+  } else if (board.includes('') === false) {
+    $('.win').text("Cats Game!");
+    console.log('Cats Game!');
+    endGame();
+  }
+};
+
+
+
+// let count = 0;
+let turns = function(index) {
+  if (board[index] === '') {
+    board[index] = currentPlayer;
+    checkWins();
+    if (currentPlayer === "X") {
+      currentPlayer = "O";
+      // checkWins();
+    } else {
+      currentPlayer = "X";
+      // checkWins();
+    }
+  } else {
+    console.log("Error");
+    $('.win').text("Cell is already taken! Pick another one!");
+  }
+};
+
+
+// let changeTurn = function() {
+//   if(currentPlayer === 'X'){
+//     currentPlayer = 'O';
+//   }else {
+//     currentPlayer = 'X';
+//   }
+// };
+
+
+// let $dude =$('#dude');
+let endGame = function() {
+  $('.box').off('click');
+};
+
+
+$('.box').on('click', function(event) {
+  if ($(event.target).text() === '') {
+    $(event.target).text(currentPlayer);
+    // $(this).addClass(player1);
+
+    // changeTurn();
+  }
+  turns(event.target.id);
+  console.log(board);
+
+});
+
+
+// $('#').on('click', function(){
+//   if($('#').text()===''){
+//     $('#').text(player1 || player2);
+//   }
+// });
+
+
+
+// $('#side').on('click', function(){
+//   const $cell = $('#' + this.id);
+//     const indx = parseInt($cell.attr('id').slice(1));
+//       $('#${indx}').html('X');
+// })
+
+// const gameShow =
+//   $('#side1').on('click', function(){
+//     alert('1');
+//   });
+
+
 
 module.exports = {
   checkWins,
   board,
-  currentPlayer,
+  resetGameBoard,
   turns,
-  resetGame
+  endGame,
+
 };
